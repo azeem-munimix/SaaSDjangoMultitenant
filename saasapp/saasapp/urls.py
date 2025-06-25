8222f3c132da46217bd7f691eb8316a1a21912a2
Azeem Sarwar <azeem.ansari@ictge.com>
2025-06-26 03:30:45 +0500

    Add tenant creation API and Docker setup

--- saasapp/saasapp/urls.py
+++ saasapp/saasapp/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from customers.views import create_customer
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/create_customer/', create_customer, name='create_customer'),
 ]
