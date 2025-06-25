e2dc023276505b470ec575fa84d56fe917bf15f7
Azeem Sarwar <azeem.ansari@ictge.com>
2025-06-26 04:09:28 +0500

    Add customer signup view

--- saasapp/saasapp/urls.py
+++ saasapp/saasapp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from customers.views import create_customer, approve_customer, list_tenants, pending_requests
+from customers.views import create_customer, approve_customer, list_tenants, pending_requests, signup
 from core.views import home, dashboard
 
 urlpatterns = [
@@ -10,6 +10,7 @@ urlpatterns = [
     path('approve/<int:pk>/', approve_customer, name='approve_customer'),
     path('api/tenants/', list_tenants, name='list_tenants'),
     path('api/requests/', pending_requests, name='pending_requests'),
+    path('signup/', signup, name='signup'),
     path('dashboard/', dashboard, name='dashboard'),
     path('', home, name='home'),
 ]
