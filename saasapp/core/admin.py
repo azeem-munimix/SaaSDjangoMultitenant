5e2e829a6dc699ec6aa7606d06f5fe17c14be8c9
Azeem Sarwar <azeem.ansari@ictge.com>
2025-06-26 06:52:53 +0500

    Add static assets and admin registrations

---
+++ saasapp/core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Item, Service, Task
+
+admin.site.register(Item)
+admin.site.register(Service)
+admin.site.register(Task)
--- saasapp/saasapp/settings.py
+++ saasapp/saasapp/settings.py
@@ -95,6 +95,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '../static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 TENANT_MODEL = 'customers.Tenant'
